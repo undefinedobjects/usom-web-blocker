b9093d8c52fcfe2a22cb91e00324b9e263d03f24
Mustafa UZUN <mustafauzun0@gmail.com>
2023-02-16 14:06:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-class Usom {
-    #usomInfo = {
-		URLList: 'https://www.usom.gov.tr/url-list.txt',
-	}
-
-    getUSOMList() {
-        return fetch(this.#usomInfo.URLList)
-        .then(list => list.text())
-        .then(list => list.split('\n'))
-        .catch(err => console.log('Failed to getList'));
-    }
-}
-
 (function() {
-	let usomURLList = [];
+	class Usom {
+		#usomInfo = {
+			URLList: 'https://www.usom.gov.tr/url-list.txt',
+		}
+	
+		getUSOMList() {
+			return fetch(this.#usomInfo.URLList)
+			.then(list => list.text())
+			.then(list => list.split('\n'))
+			.catch(err => console.log('Failed to getList'));
+		}
+	}
+	
+	const isIpAddress = (ipAddress) => /^((\d){1,3}\.){3}(\d){1,3}$/.test(ipAddress)
+
+
+	let usomURLList;
 	chrome.runtime.onInstalled.addListener( () => {
 		if (navigator.onLine) {
-			usomURLList = new Usom().getUSOMList();
+			usomURLList = new Usom().getUSOMList().then(list => list);
 		}
 	});
 
 	chrome.tabs.onUpdated.addListener(function(tabid, info, tab) {
 		if(info?.url) {
-			console.log(tabid, new URL(info.url).hostname, usomURLList)
-			
+			let url = new URL(info.url);
+
+			if(!isIpAddress(url)) {
+				url = url.hostname.split('.');
+				url.shift();
+				url = url.join('.');
+			}
+
+			usomURLList.then(list => {
+				if(!list.includes(url)) {
+					return;
+				}
+				
+				chrome.declarativeNetRequest.updateDynamicRules({
+					addRules:[{
+					   'id': 1,
+					   'priority': 1,
+					   'action': { 'type': 'block' },
+					   'condition': { 'urlFilter': url, 'resourceTypes': ['main_frame'] }
+					}],
+					removeRuleIds: [1]
+				})			
+			})
 		}
 	});
 })();
